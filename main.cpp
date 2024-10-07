014d85780c29c6fcc6faffa0bc00ff5010257783
Clon54 <clon5454@outlook.com>
2024-10-07 18:31:28 -0500

    Testeando el comportamiendo de la nueva funcion.

--- main.cpp
+++ main.cpp
@@ -9,6 +9,8 @@
 int main(int argc, char const *argv[]) {
 
     TestAscciAtText();
+
+    std::cout << "Convert My is at ascci secunce: " << ConvertStrintAtAscci("My is") << '\n';
     
 
   return 0;
